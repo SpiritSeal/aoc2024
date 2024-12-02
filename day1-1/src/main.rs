bd87bd3a1a64a277b4b60ce98f4f4b85d2e72601
Daniel Gallups <dsgallups@protonmail.com>
2024-12-02 17:18:48 -0500

    review

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,35 +1,24 @@
-use std::fs::File;
 use std::io::prelude::*;
 use std::iter::zip;
-use std::path::Path;
 
 fn main() {
-    let path = Path::new("./input.txt");
-    let display = path.display();
-    
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read bc {}", why),
-        Ok(_) => (),
-    };
+    //dan -- include_bytes! removes a lot of boilerplate for FS. Includes the bytes into the binary at comp time.
+    let s = include_bytes!("../input.txt");
 
     // create vectors from each of the columns
-    let mut v1:Vec<i32> = Vec::new();
-    let mut v2:Vec<i32> = Vec::new();
+    let mut v1: Vec<i32> = Vec::new();
+    let mut v2: Vec<i32> = Vec::new();
 
-    let l1: Vec<&str> = s.lines().collect();
+    // dan -- can only collect into owned type, shouldn't make a different
+    let l1: Vec<String> = s.lines().map(|line| line.unwrap()).collect();
 
     for line_val in l1 {
-        let x:Vec<i32> = line_val.split_whitespace()
-            .filter_map(|x| x.parse().ok())
+        let x: Vec<i32> = line_val
+            .split_whitespace()
+            //dan -- unwrap, filter_mapping on error can cause a real headache
+            .map(|x| x.parse().unwrap())
             .collect();
-        
+
         v1.push(x[0]);
         v2.push(x[1]);
     }
@@ -41,16 +30,18 @@ fn main() {
     // dbg!(&v1.len());
     // dbg!(&v2.len());
 
-
-    let pairs:Vec<(i32, i32)> = zip(v1, v2).collect();
+    let pairs: Vec<(i32, i32)> = zip(v1, v2).collect();
     // dbg!(&z);
 
-    let output: u32 = pairs.into_iter()
-        .filter_map(|x| Some((x.0 - x.1).abs()))
+    let output: u32 = pairs
+        .into_iter()
+        //dan -- I think this was overlooked, just gonna fix
+        .map(|x| (x.0 - x.1).abs())
         .fold(0u32, |sum, i| sum + (i as u32));
 
-    println!("
-    Answer: {}", output);
-
-
+    println!(
+        "
+    Answer: {}",
+        output
+    );
 }
