01ecea4dfd5bf450428fa068f65b904753f40dcc
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2024-12-02 17:11:21 -0500

    cleanup day 1 submission

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -4,11 +4,9 @@ use std::iter::zip;
 use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
     let path = Path::new("./input.txt");
     let display = path.display();
     
-    // println!("{}", display);
 
     let mut file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", display, why),
@@ -25,29 +23,23 @@ fn main() {
     let mut v1:Vec<i32> = Vec::new();
     let mut v2:Vec<i32> = Vec::new();
 
-    // let l1: Vec<&str> = s.split("\r\n").collect().split(" ").collect();
     let l1: Vec<&str> = s.lines().collect();
-    // println!("{:?}", l1);
 
     for line_val in l1 {
-        // dbg!(line_val);
         let x:Vec<i32> = line_val.split_whitespace()
             .filter_map(|x| x.parse().ok())
             .collect();
-        // dbg!(&x);
         
         v1.push(x[0]);
         v2.push(x[1]);
-        
-        // println!();
     }
 
     v1.sort();
     v2.sort();
 
-    // both are 1000 elements
-    dbg!(&v1.len());
-    dbg!(&v2.len());
+    // both are 1000 elements btw
+    // dbg!(&v1.len());
+    // dbg!(&v2.len());
 
 
     let pairs:Vec<(i32, i32)> = zip(v1, v2).collect();
@@ -57,7 +49,8 @@ fn main() {
         .filter_map(|x| Some((x.0 - x.1).abs()))
         .fold(0u32, |sum, i| sum + (i as u32));
 
-    println!("Output: {}", output);
+    println!("
+    Answer: {}", output);
 
 
 }
