b577b83003e6a8776199055aaf472f459ea09a17
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2024-12-02 18:30:29 -0500

    init day 2

---
+++ day2/target/debug/deps/day2-bfbcf8c9b0e0b4ba.d
@@ -0,0 +1,5 @@
+C:\Users\spyre\Documents\code\aoc-24\day2\target\debug\deps\libday2-bfbcf8c9b0e0b4ba.rmeta: src/main.rs
+
+C:\Users\spyre\Documents\code\aoc-24\day2\target\debug\deps\day2-bfbcf8c9b0e0b4ba.d: src/main.rs
+
+src/main.rs:
