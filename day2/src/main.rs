ce5987d399728256fbe10a2c1d2f794cc11745d4
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2024-12-04 17:16:55 -0500

    clean up day 2 pt 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,8 +4,6 @@
 // }
 
 fn main() {
-    println!("Hello, world!");
-
     let input = include_str!("../input.txt");
     // let input = include_str!("../test.txt");
 
@@ -24,25 +22,12 @@ fn main() {
             .collect::<Vec<i32>>()
         )
         .collect::<Vec<Vec<i32>>>()
-        // .windows(2)
     ;
 
-    // dbg!(&stage1);
-
     let increasing_count = stage1
-        // .collect::<Vec<Vec<i32>>>()  // Collect into Vec<Vec<i32>> first
         .clone()
         .into_iter()
         .filter(|report| {
-            // // Compare each pair of vectors
-            // let current = &report[0];
-            // let next = &report[1];
-            
-            // // Check if every element in next is greater than corresponding element in current
-            // current.len() == next.len() && 
-            // current.iter()
-            //     .zip(next.iter())
-            //     .all(|(curr, nxt)| (nxt > curr) && (*nxt <= *curr + 3))
             report
                 .into_iter()
                 .all(|val| *val > 0 && *val <= 3)
@@ -53,18 +38,8 @@ fn main() {
 
     let decreasing_count = stage1
         .clone()
-        // .collect::<Vec<Vec<i32>>>()  // Collect into Vec<Vec<i32>> first
         .into_iter()
         .filter(|report| {
-            // // Compare each pair of vectors
-            // let current = &report[0];
-            // let next = &report[1];
-            
-            // // Check if every element in next is greater than corresponding element in current
-            // current.len() == next.len() && 
-            // current.iter()
-            //     .zip(next.iter())
-            //     .all(|(curr, nxt)| (nxt > curr) && (*nxt <= *curr + 3))
             report
                 .into_iter()
                 .all(|val| *val < 0 && *val >= -3)
@@ -73,14 +48,8 @@ fn main() {
         // .collect::<Vec<Vec<i32>>>()
     ;
 
-
-    // .collect::<Vec<Vec<u32>>>()
-    // dbg!(stage1.clone());
-    // dbg!(increasing_count);
-    // dbg!(decreasing_count);
-    let answer = increasing_count + decreasing_count;
-    dbg!(answer);
-    // dbg!(&output.clone());
+    let part_1_answer = increasing_count + decreasing_count;
+    println!("Part 1 Answer: {}", part_1_answer)
 
 }
 
